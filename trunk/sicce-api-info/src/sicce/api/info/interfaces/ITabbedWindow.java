cedbcce0ff9da37e52c745f413aae71ba20e9cec
gishac <gishac@c6e2a2cf-2f43-0410-996f-8b6ab9ff9bea>
2008-01-08 08:42:04 +0000

    Agregada interfaz ITabbedWindow que sera implementada por cada mantenimiento de la aplicacion

---
+++ trunk/sicce-api-info/src/sicce/api/info/interfaces/ITabbedWindow.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sicce.api.info.interfaces;
+
+/**
+ *
+ * @author gish@c
+ */
+public interface ITabbedWindow {
+
+    void setTitle(String title);
+    String getTitle();
+}
