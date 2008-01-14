87f6b92ade4bc5970b4843105e6933b0dbe57302
gishac <gishac@c6e2a2cf-2f43-0410-996f-8b6ab9ff9bea>
2008-01-14 04:55:57 +0000

    Creacion de la clase ConstantsProvider y TaskInfo

---
+++ sicce-api-info/src/sicce/api/info/ConstantsProvider.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sicce.api.info;
+
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
+/**
+ *
+ * @author gish@c
+ */
+public class ConstantsProvider {
+
+    public enum TasksProvider
+    {
+        User,
+        PowerMeter,
+        Location
+    }
+}
---
+++ sicce-api-info/src/sicce/api/info/TaskInfo.java
@@ -0,0 +1,70 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sicce.api.info;
+
+import java.util.ArrayList;
+import java.util.List;
+import sicce.api.info.ConstantsProvider.TasksProvider;
+
+/**
+ *
+ * @author gish@c
+ */
+public class TaskInfo {
+
+    private String name;
+    private String iconName;
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIconName() {
+        return iconName;
+    }
+
+    public void setIconName(String iconName) {
+        this.iconName = iconName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public TaskInfo() {
+    }
+    
+    public TaskInfo(String name, String description, String iconName) {
+        this.name = name;
+        this.description = description;
+        this.iconName = iconName;
+    }
+    
+    /**
+     * Crea la lista de tareas para el panel 
+     * @return
+     */
+    public static List<TaskInfo> getTasks()
+    {
+        List<TaskInfo> tasks = new ArrayList<TaskInfo>();
+        for(TasksProvider task : TasksProvider.values())
+        {
+            TaskInfo taskInfo = new TaskInfo(task.toString(),"Description" + task.toString(),"Icon" + task.toString());
+            tasks.add(taskInfo);
+        }
+        return tasks;
+    }
+    
+}
