92b78b6e04746e109e98a2b7f817e9385a02f405
gishac <gishac@c6e2a2cf-2f43-0410-996f-8b6ab9ff9bea>
2008-02-01 05:21:20 +0000

    Creacion de objeto para manejo de conexion a la base de datos Objeto RoleDB para manejar el acceso a datos de los roles

---
+++ trunk/sicce-api-dataaccess/src/sicce/api/dataaccess/RoleDB.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sicce.api.dataaccess;
+import sicce.api.info.Role;
+import sicce.api.info.interfaces.IRole;
+
+/**
+ *
+ * @author gish@c
+ */
+public class RoleDB {
+
+    public static IRole Save(IRole role) throws Exception
+    {
+        try
+        {
+            
+        Role roleToSave = (Role)Connection.getDataContext().newObject(Role.class);
+        roleToSave.setDescription(role.getDescription());        
+        Connection.getDataContext().commitChanges();
+        return roleToSave;
+        }
+        catch(Exception ex)
+        {
+            throw ex;
+        }
+    }
+    
+    public static void Update(IRole role) throws Exception
+    {
+        try
+        {
+            Connection.getDataContext().modifiedObjects();        
+            Connection.getDataContext().commitChanges();
+        }
+        catch(Exception ex)
+        {
+            throw ex;
+        }
+    }
+    
+}
