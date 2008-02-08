d8d867199552503096495a98b3e141b8294da0d7
gishac <gishac@c6e2a2cf-2f43-0410-996f-8b6ab9ff9bea>
2008-02-08 07:07:01 +0000

    Interfaz IUser agregada Nueva enumeracion

--- sicce-api-info/src/sicce/api/info/UserSicce.java
+++ sicce-api-info/src/sicce/api/info/UserSicce.java
@@ -1,8 +1,18 @@
 package sicce.api.info;
 
 import sicce.api.info.auto._UserSicce;
+import sicce.api.info.interfaces.IRole;
+import sicce.api.info.interfaces.IUserSicce;
 
-public class UserSicce extends _UserSicce {
+public class UserSicce extends _UserSicce implements IUserSicce {
+
+    public IRole getRole() {
+        return getToRole();
+    }
+
+    public void setRole(IRole role) {
+        setToRole((Role)role);
+    }
 
 }
 
