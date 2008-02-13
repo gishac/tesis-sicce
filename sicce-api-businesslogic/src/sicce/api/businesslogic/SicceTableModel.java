c0089b298e9ce15f65dde10e478cbd181cf16d83
gishac <gishac@c6e2a2cf-2f43-0410-996f-8b6ab9ff9bea>
2008-02-13 04:34:41 +0000

    SicceTableModel actualizado

--- sicce-api-businesslogic/src/sicce/api/businesslogic/SicceTableModel.java
+++ sicce-api-businesslogic/src/sicce/api/businesslogic/SicceTableModel.java
@@ -10,7 +10,7 @@ import javax.swing.table.AbstractTableModel;
 import javax.swing.table.TableColumn;
 
 /**
- *
+ * Modelo generico para el manejo de JTables
  * @author gish@c
  */
 public abstract class SicceTableModel<T> extends AbstractTableModel {
@@ -18,6 +18,10 @@ public abstract class SicceTableModel<T> extends AbstractTableModel {
     List<T> dataSource;
     protected String[] columns = null;
 
+    /**
+     * Devuelve la fuente de datos asignada para el modelo
+     * @return
+     */
     protected List<T> getDataSource() {
         if (dataSource == null) {
             dataSource = new ArrayList<T>();
@@ -25,30 +29,61 @@ public abstract class SicceTableModel<T> extends AbstractTableModel {
         return dataSource;
     }
 
+    /**
+     * Constructor
+     */
     public SicceTableModel() {
     }
 
+    /**
+     * Constructor
+     * @param dataSource Fuente de datos para el modelo
+     */
     public SicceTableModel(List<T> dataSource) {
         this.dataSource = dataSource;
     }
 
+    /**
+     * 
+     * @return El numero de elementos a mostrar en la tabla
+     */
     public int getRowCount() {
         return getDataSource().size();
     }
 
+    /**
+     * 
+     * @return El numero de columnas a mostrar en la tabla
+     */
     public int getColumnCount() {
         return columns.length;
     }
 
+    /**
+     * Devuelve el nombre de la columna en la tabla
+     * @param column Indice de la columna
+     * @return
+     */
     @Override
     public String getColumnName(int column) {
         return columns[column];
     }
 
+    /**
+     * Devuelve el valor en la celda en la fila y columna 
+     * @param rowIndex Numero de fila
+     * @param columnIndex Numero de columna
+     * @return
+     */
     public Object getValueAt(int rowIndex, int columnIndex) {
         throw new UnsupportedOperationException("The method is not implemented");
     }
 
+    /**
+     * Devuelve el objeto cargado en la fila
+     * @param index Fila del objeto a leer
+     * @return
+     */
     public T getRow(int index) {
         if (getRowCount() > index) {
             return getDataSource().get(index);
@@ -57,14 +92,14 @@ public abstract class SicceTableModel<T> extends AbstractTableModel {
     }
      /**
      * Ajusta el Tamaño de una columna específica, dentro de un jgrid
-     * @param pColumna Posición de la Columna a Ajustar
-     * @param pTamanio Tamaño de la columna.
+     * @param column Columna a Ajustar
+     * @param size Tamaño de la columna.
      */
-    public static void ColumnSize(TableColumn pColumna, int pTamanio)
+    public static void ColumnSize(TableColumn column, int size)
     {
-        pColumna.setMinWidth(pTamanio);
-        pColumna.setPreferredWidth(pTamanio);
-        pColumna.setResizable(true);
+        column.setMinWidth(size);
+        column.setPreferredWidth(size);
+        column.setResizable(true);
     };
     
 }
