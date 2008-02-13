1d3e8772acedc19b7aac86d3ddee6450728f6b44
gishac <gishac@c6e2a2cf-2f43-0410-996f-8b6ab9ff9bea>
2008-02-13 06:52:37 +0000

    Modificaciones en Role, OptionSicce Agregada interfaz IOptionRole

--- trunk/sicce-api-info/src/sicce/api/info/OptionSicce.java
+++ trunk/sicce-api-info/src/sicce/api/info/OptionSicce.java
@@ -5,6 +5,12 @@ import sicce.api.info.interfaces.IOptionSicce;
 
 public class OptionSicce extends _OptionSicce implements IOptionSicce {
 
+    public static final String OPTION_ID = "ID_OPTION_SICCE";
+    
+    public int getID() {
+        return (getObjectId() != null)? Integer.parseInt(getObjectId().getIdSnapshot().get(OPTION_ID).toString()) : 0;
+    }
+
     
     
 }
--- trunk/sicce-api-info/src/sicce/api/info/Role.java
+++ trunk/sicce-api-info/src/sicce/api/info/Role.java
@@ -1,17 +1,43 @@
 package sicce.api.info;
 
+import java.util.List;
+import org.apache.cayenne.ObjectId;
 import sicce.api.info.auto._Role;
+import sicce.api.info.interfaces.IOptionRole;
 import sicce.api.info.interfaces.IRole;
 
 public class Role extends _Role implements IRole {
 
     public static final String ROLE_ID = "ID_ROLE";
-    
-    public int getID()
-    {
-        return (getObjectId() != null)? Integer.parseInt(getObjectId().getIdSnapshot().get(ROLE_ID).toString()) : 0;
+
+    public int getID() {
+        return (getObjectId() != null) ? Integer.parseInt(getObjectId().getIdSnapshot().get(ROLE_ID).toString()) : 0;
     }
-    
+
+    public List<IOptionRole> getPermissions() {
+        return getOptionRoleArray();
+    }
+
+    public void generateID() {
+        this.setObjectId(new ObjectId(Role.class.getName()));
+    }
+
+    @Override
+    public Object clone() {
+        Role role = new Role();
+        role.setDescription(this.getDescription());
+        role.setID(this.getObjectId());
+        if (this.getPermissions() != null) {
+            for (IOptionRole permission : this.getPermissions()) {
+                role.addToOptionRoleArray((OptionRole) permission);
+            }
+        }
+        return role;
+    }
+
+    public void setID(Object id) {
+        this.setObjectId((ObjectId) id);
+    }
 }
 
 
