c3e8368478a27a4f40a1c3780ef1c2ab8348a1ff
gishac <gishac@c6e2a2cf-2f43-0410-996f-8b6ab9ff9bea>
2008-03-14 05:28:48 +0000

    Cambio de columnas a mostrar en las tablas

--- trunk/sicce-api-businesslogic/src/sicce/api/businesslogic/LocationTableModel.java
+++ trunk/sicce-api-businesslogic/src/sicce/api/businesslogic/LocationTableModel.java
@@ -17,7 +17,7 @@ public class LocationTableModel extends SicceTableModel<ILocation> {
 
     public LocationTableModel(List<ILocation> dataSource) {
         this.dataSource = dataSource;
-        columns = new String[]{"No.", "Ubicación", "Tipo de Dependencia", "Medidor Asignado" , "Descripción" };
+        columns = new String[]{"No.", "Descripción", "Tipo de Dependencia", "Medidor Asignado" , "Ubicación" };
     }
 
     @Override
@@ -28,16 +28,16 @@ public class LocationTableModel extends SicceTableModel<ILocation> {
             case 0:
                 return rowIndex + 1;
             case 1: 
-                if (location.getLocation()!=null)
-                    return location.getLocation().getDescription();
-                else
-                    return null;
+                return location.getDescription();
             case 2:
                 return location.getLocationType().getDescription();
             case 3:
                 return location.getPowerMeter().getDescription();
             case 4:
-                return location.getDescription();
+                if (location.getLocation()!=null)
+                    return location.getLocation().getDescription();
+                else
+                    return null;                
             default: 
                 return null;
         }
