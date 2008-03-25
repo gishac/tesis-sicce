e91791f6bf6916ab260d634f0320e4689f682826
gishac <gishac@c6e2a2cf-2f43-0410-996f-8b6ab9ff9bea>
2008-03-25 04:44:04 +0000

    Actualizacion en el script

--- sicce-api-dataaccess/SQLScripts/DatabaseScript.sql
+++ sicce-api-dataaccess/SQLScripts/DatabaseScript.sql
@@ -36,13 +36,14 @@ CREATE TABLE UNIT_MEASURE (
 
 CREATE TABLE OPTION_SICCE (
   ID_OPTION_SICCE INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  DESCRIPTION VARCHAR(30) NULL,
+  DESCRIPTION VARCHAR(50) NULL,
+  ACTION_COMMAND VARCHAR(50) NULL,
   ICON BLOB NULL,
   PRIMARY KEY(ID_OPTION_SICCE)
 );
 
 CREATE TABLE LOCATION_TYPE (
-  ID_LOCATION_TYPE INTEGER UNSIGNED NOT NULL AUTO_INCREMENT
+  ID_LOCATION_TYPE INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
   DESCRIPTION VARCHAR(40) NOT NULL,
   PRIMARY KEY(ID_LOCATION_TYPE)
 );
@@ -54,7 +55,7 @@ CREATE TABLE USER_SICCE (
   LASTNAME VARCHAR(20) NULL,
   CODE_UCSG INTEGER UNSIGNED NULL,
   USERNAME_SICCE VARCHAR(20) NULL,
-  PASSWORD_SICCE VARCHAR(20) NULL,
+  PASSWORD_SICCE VARCHAR(100) NULL,
   PRIMARY KEY(ID_USER_SICCE),
   INDEX USER_SICCE_FKIndex1(ID_ROLE),
   FOREIGN KEY(ID_ROLE)
@@ -132,4 +133,23 @@ CREATE TABLE MEASURE (
       ON UPDATE NO ACTION
 );
 
+insert into option_sicce (ID_OPTION_SICCE,DESCRIPTION,ICON,ACTION_COMMAND) values(0,'Roles',null,'Role');
+insert into option_sicce (ID_OPTION_SICCE,DESCRIPTION,ICON,ACTION_COMMAND) values(0,'Usuarios',null,'User');
+insert into option_sicce (ID_OPTION_SICCE,DESCRIPTION,ICON,ACTION_COMMAND) values(0,'Medidores',null,'PowerMeter');
+insert into option_sicce (ID_OPTION_SICCE,DESCRIPTION,ICON,ACTION_COMMAND) values(0,'Tipos de Dependencia',null,'LocationType');
+insert into option_sicce (ID_OPTION_SICCE,DESCRIPTION,ICON,ACTION_COMMAND) values(0,'Zonas',null,'Zone');
+insert into option_sicce (ID_OPTION_SICCE,DESCRIPTION,ICON,ACTION_COMMAND) values(0,'Parámetros',null,'Parameter');
+insert into option_sicce (ID_OPTION_SICCE,DESCRIPTION,ICON,ACTION_COMMAND) values(0,'Dependencias',null,'Location');
+
+insert into role values(0,'Administrador');
+insert into user_sicce(ID_USER_SICCE,ID_ROLE,NAME,LASTNAME,CODE_UCSG,USERNAME_SICCE,PASSWORD_SICCE) values(0,1,'adminsicce','adminsicce',null,'adminsicce','BGEYVgYT0cCYB6SDjstB8+09gzaB/ZW/');
+insert into option_role values(1,1);
+insert into option_role values(1,2);
+insert into option_role values(1,3);
+insert into option_role values(1,4);
+insert into option_role values(1,5);
+insert into option_role values(1,6);
+insert into option_role values(1,7);
+
+insert into location_type values(0,'Edificio');
 
