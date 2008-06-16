a06104be49c7806b176cd3d291e38b077ba1480f
gishac <gishac@c6e2a2cf-2f43-0410-996f-8b6ab9ff9bea>
2008-06-16 05:22:03 +0000

    Mapeo de tabla de reportes

---
+++ trunk/sicce-api-dataaccess/src/sicce/api/dataaccess/ScheduledDayDB.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sicce.api.dataaccess;
+
+import java.util.Set;
+import sicce.api.info.interfaces.IScheduleDay;
+
+/**
+ *
+ * @author gish@c
+ */
+public class ScheduledDayDB {
+
+    public static void Save(Set<IScheduleDay> schedules) throws Exception {
+        try {
+            for(IScheduleDay schedule : schedules){
+                Save(schedule);
+            }
+        } catch (Exception ex) {
+            throw ex;
+        }
+    }
+
+    public static void Save(IScheduleDay schedule) throws Exception {
+        try {
+
+            DataAccessManager.getInstance().getScheduleDayDB().save(schedule);
+        } catch (Exception ex) {
+            throw ex;
+        }
+    }
+
+    public static void Update(IScheduleDay schedule) throws Exception {
+        try {
+            DataAccessManager.getInstance().getScheduleDayDB().update(schedule);
+        } catch (Exception ex) {
+            throw ex;
+        }
+    }
+
+    public static void Delete(IScheduleDay schedule) throws Exception {
+        try {
+            DataAccessManager.getInstance().getScheduleDayDB().delete(schedule);
+        } catch (Exception ex) {
+            throw ex;
+        }
+    }
+}
