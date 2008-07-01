07e2e27b1b3214506dc5dd58cf6d667f3a386d4f
gishac <gishac@c6e2a2cf-2f43-0410-996f-8b6ab9ff9bea>
2008-07-01 04:04:13 +0000

    Actualizaciones para soporte de lectura de mediciones

--- trunk/sicce-api-businesslogic/src/sicce/api/businesslogic/ClassFactory.java
+++ trunk/sicce-api-businesslogic/src/sicce/api/businesslogic/ClassFactory.java
@@ -7,19 +7,27 @@ package sicce.api.businesslogic;
 import sicce.api.info.Alarm;
 import sicce.api.info.Location;
 import sicce.api.info.LocationType;
+import sicce.api.info.Measure;
+import sicce.api.info.ModbusRequest;
+import sicce.api.info.ModbusResponse;
 import sicce.api.info.OptionSicce;
 import sicce.api.info.PowerMeter;
 import sicce.api.info.Report;
 import sicce.api.info.Role;
+import sicce.api.info.ScheduleDay;
 import sicce.api.info.UserSicce;
 import sicce.api.info.Zone;
 import sicce.api.info.interfaces.IAlarm;
 import sicce.api.info.interfaces.ILocation;
 import sicce.api.info.interfaces.ILocationType;
+import sicce.api.info.interfaces.IMeasure;
+import sicce.api.info.interfaces.IModbusRequest;
+import sicce.api.info.interfaces.IModbusResponse;
 import sicce.api.info.interfaces.IOptionSicce;
 import sicce.api.info.interfaces.IPowerMeter;
 import sicce.api.info.interfaces.IReport;
 import sicce.api.info.interfaces.IRole;
+import sicce.api.info.interfaces.IScheduleDay;
 import sicce.api.info.interfaces.IUserSicce;
 import sicce.api.info.interfaces.IZone;
 
@@ -59,6 +67,22 @@ public class ClassFactory {
     
     public static IAlarm getAlarmInstance(){
         return new Alarm();
+    }
+    
+    public static IScheduleDay getScheduleInstance(){
+        return new ScheduleDay();
+    }
+    
+    public static IModbusRequest getModbusRequest(){
+        return new ModbusRequest();
+    }
+    
+    public static IModbusResponse getModbusResponse(){
+        return new ModbusResponse();
+    }
+    
+    public static IMeasure getMeasure(){
+        return new Measure();
     }
     
     public static IReport getReportInstance(){
--- trunk/sicce-api-businesslogic/src/sicce/api/businesslogic/PowerMeterBizObject.java
+++ trunk/sicce-api-businesslogic/src/sicce/api/businesslogic/PowerMeterBizObject.java
@@ -4,8 +4,16 @@
  */
 package sicce.api.businesslogic;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.util.HashMap;
 import java.util.List;
 import sicce.api.dataaccess.PowerMeterDB;
+import sicce.api.info.ConstantsProvider.RequestType;
+import sicce.api.info.exceptions.InvalidModbusResponseException;
+import sicce.api.info.interfaces.IMeasure;
+import sicce.api.info.interfaces.IModbusRequest;
+import sicce.api.info.interfaces.IModbusResponse;
 import sicce.api.info.interfaces.IPowerMeter;
 
 /**
@@ -14,7 +22,40 @@ import sicce.api.info.interfaces.IPowerMeter;
  */
 public class PowerMeterBizObject {
 
+    /**
+     * 
+     * @return
+     */
     public List<IPowerMeter> GetAllPowerMeter() {
-        return PowerMeterDB.GetAllPowerMeter();        
+        return PowerMeterDB.GetAllPowerMeter();
     }
+
+    /**
+     * 
+     * @param powerMeter
+     * @return
+     * @throws java.net.UnknownHostException
+     * @throws java.io.IOException
+     * @throws java.lang.Exception
+     */
+    public HashMap<RequestType,IModbusResponse> ReadPowerMeterData(IPowerMeter powerMeter) throws UnknownHostException, IOException, Exception {
+        ModbusBizObject modbusHandler = new ModbusBizObject();
+        IModbusRequest readHoldingRegistersRequest = modbusHandler.BuildModbusRequestToReadHoldingRegisters(powerMeter);
+        IModbusResponse response = modbusHandler.SendModbusRequest(readHoldingRegistersRequest, RequestType.HoldingRegisters,powerMeter);
+        HashMap<RequestType,IModbusResponse> result = new HashMap<RequestType, IModbusResponse>();
+        result.put(RequestType.HoldingRegisters, response);
+        return result;
+    }
+    
+    /**
+     * 
+     * @param powerMeterData
+     */
+    public void ProcessPowerMeterData(HashMap<RequestType,IModbusResponse> powerMeterData) throws InvalidModbusResponseException{
+        IMeasure measure = ClassFactory.getMeasure();
+        IModbusResponse response = powerMeterData.get(RequestType.HoldingRegisters);
+        System.out.println(response.getHoldingRegistersResponse());
+        new ModbusBizObject().ProcessModbusResponse(response, measure);
+    }
+    
 }
