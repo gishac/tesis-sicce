81c716c32c4e9f0ca55127f81a8dcb4fd9cc6314
gishac <gishac@c6e2a2cf-2f43-0410-996f-8b6ab9ff9bea>
2008-07-01 04:08:25 +0000

    Actualizaciones para soporte de lectura de mediciones

--- trunk/sicce-api-processor/src/sicce/api/processor/Processor.java
+++ trunk/sicce-api-processor/src/sicce/api/processor/Processor.java
@@ -8,6 +8,7 @@ package sicce.api.processor;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Observer;
+import sicce.api.businesslogic.ClassFactory;
 import sicce.api.businesslogic.PowerMeterBizObject;
 import sicce.api.info.interfaces.IPowerMeter;
 import sicce.api.info.interfaces.IPowerMeterWatcher;
@@ -20,6 +21,10 @@ import sicce.api.util.EncryptionProvider;
 public class Processor {
 
     private static List<Observer> observers;
+    /**
+     * Devuelve los observadores registrados
+     * @return
+     */
     public static List<Observer> getObservers()
     {
         if(observers == null){
@@ -29,25 +34,7 @@ public class Processor {
     }
     
     /**
-     * Realiza el proceso de lectura de los medidores
-     */
-    public static void DoProcess(){
-        EncryptionProvider.RegisterHibernateEncryptor();
-        int readInterval = 3;
-        PowerMeterBizObject powerMeterHandler = new PowerMeterBizObject();
-        List<IPowerMeter> powerMeters = powerMeterHandler.GetAllPowerMeter();
-        ArrayList<IPowerMeterWatcher> watchers = new ArrayList<IPowerMeterWatcher>();
-        for (IPowerMeter powerMeter : powerMeters) {
-            IPowerMeterWatcher watcher = new PowerMeterWatcher(powerMeter);
-            SetObservers(watcher);        
-            watchers.add(watcher);            
-        }
-        TimerTaskLauncher taskLauncher = new TimerTaskLauncher(watchers, readInterval);
-        taskLauncher.BeginTasks();
-    }
-    
-    /**
-     * 
+     * Registra un observador para los controladores de medidores
      * @param observer
      */
     public static void AddObserver(Observer observer){
@@ -55,7 +42,7 @@ public class Processor {
     }
     
     /**
-     * 
+     * Setea los observadores a los controladores de medidor
      * @param watcher
      */
     private static void SetObservers(IPowerMeterWatcher watcher){
@@ -64,4 +51,30 @@ public class Processor {
         }
     }
     
+    /**
+     * Realiza el proceso de lectura de los medidores
+     */
+    public static void DoProcess(){
+        EncryptionProvider.RegisterHibernateEncryptor();
+        int readInterval = 5;
+        PowerMeterBizObject powerMeterHandler = new PowerMeterBizObject();
+        List<IPowerMeter> powerMeters = powerMeterHandler.GetAllPowerMeter();
+        
+        IPowerMeter meter = ClassFactory.getPowerMeterInstance();
+        meter.setIpAddress("192.168.8.42");
+        meter.setDescription("home test");
+        meter.setSerial("01");
+        meter.setDeviceID("01");
+        
+        powerMeters.add(meter);
+        
+        ArrayList<IPowerMeterWatcher> watchers = new ArrayList<IPowerMeterWatcher>();
+        for (IPowerMeter powerMeter : powerMeters) {
+            IPowerMeterWatcher watcher = new PowerMeterWatcher(powerMeter);
+            SetObservers(watcher);        
+            watchers.add(watcher);            
+        }
+        TimerTaskLauncher taskLauncher = new TimerTaskLauncher(watchers, readInterval);
+        taskLauncher.BeginTasks();
+    }    
 }
