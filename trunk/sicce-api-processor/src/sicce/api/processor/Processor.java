96936bae7fcd7e72951118324fab44fc621cd903
gishac <gishac@c6e2a2cf-2f43-0410-996f-8b6ab9ff9bea>
2008-07-21 00:25:06 +0000

    Soporte para nueva vista de datos por medidor

--- trunk/sicce-api-processor/src/sicce/api/processor/Processor.java
+++ trunk/sicce-api-processor/src/sicce/api/processor/Processor.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package sicce.api.processor;
 
 import java.util.ArrayList;
@@ -10,7 +9,6 @@ import java.util.List;
 import java.util.Observer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import sicce.api.businesslogic.factory.ClassFactory;
 import sicce.api.businesslogic.PowerMeterBizObject;
 import sicce.api.dataaccess.ParameterDB;
 import sicce.api.info.ConstantsProvider;
@@ -26,43 +24,42 @@ import sicce.api.util.EncryptionProvider;
 public class Processor {
 
     private static List<Observer> observers;
+
     /**
      * Devuelve los observadores registrados
      * @return
      */
-    public static List<Observer> getObservers()
-    {
-        if(observers == null){
+    public static List<Observer> getObservers() {
+        if (observers == null) {
             observers = new ArrayList<Observer>();
         }
         return observers;
     }
-    
+
     /**
      * Registra un observador para los controladores de medidores
      * @param observer
      */
-    public static void AddObserver(Observer observer){
+    public static void AddObserver(Observer observer) {
         getObservers().add(observer);
     }
-    
+
     /**
      * Setea los observadores a los controladores de medidor
      * @param watcher
      */
-    private static void SetObservers(IPowerMeterWatcher watcher){
-        for(Observer observer : getObservers()){
+    private static void SetObservers(IPowerMeterWatcher watcher) {
+        for (Observer observer : getObservers()) {
             watcher.AddObserver(observer);
         }
     }
-   
-    
+
     /**
      * Realiza el proceso de lectura de los medidores
      */
-    public static void DoProcess(){
+    public static void DoProcess() {
         try {
-            
+
             EncryptionProvider.RegisterHibernateEncryptor();
             IParameter readInterval = ParameterDB.GetParameterByKey(ConstantsProvider.READ_INTERVAL);
             PowerMeterBizObject powerMeterHandler = new PowerMeterBizObject();
@@ -78,5 +75,5 @@ public class Processor {
         } catch (Exception ex) {
             Logger.getLogger(Processor.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }    
+    }
 }
