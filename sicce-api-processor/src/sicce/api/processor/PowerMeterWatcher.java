75a2cdd66d3efce3b4dea3b115876c305e81af32
gishac <gishac@c6e2a2cf-2f43-0410-996f-8b6ab9ff9bea>
2008-07-21 00:25:06 +0000

    Soporte para nueva vista de datos por medidor

--- sicce-api-processor/src/sicce/api/processor/PowerMeterWatcher.java
+++ sicce-api-processor/src/sicce/api/processor/PowerMeterWatcher.java
@@ -63,34 +63,6 @@ public class PowerMeterWatcher extends Observable implements IPowerMeterWatcher 
      */
     public void Watch() {
         try {
-//            //Lee los datos del medidor y los guarda en la base\
-//            String[] ip = this.powerMeter.getIpAddress().split(".");
-//            byte[] ipb = new byte[4];
-//            ipb[0] = (byte)192;
-//            ipb[1] = (byte)168;
-//            ipb[2] = (byte)8;
-//            ipb[3] = (byte)42;
-//
-//            InetAddress ipaddress = InetAddress.getByAddress(this.powerMeter.getDescription(), ipb);
-//            Socket xocket = new Socket(ipaddress, 2101);
-//
-//            OutputStream output = xocket.getOutputStream();
-//            InputStream input = xocket.getInputStream();
-//            BufferedInputStream binput = new BufferedInputStream(xocket.getInputStream());
-//            String[] trama = {"70", "01", "03", "03", "00", "00", "62", "C4", "67"};
-//            trama[1] = this.powerMeter.getSerial();
-//
-//            write(output, trama);
-//
-//
-//            read(xocket.getInputStream(), 200);
-//
-//            xocket.close();
-
-
-            /*int lectura = new Random().nextInt(10000);
-            setChanged();
-            notifyObservers(lectura);*/
             
             HashMap<RequestType, IModbusResponse> powerMeterData = powerMeterBizObject.ReadPowerMeterData(this.powerMeter);
             IMeasure measure = powerMeterBizObject.ProcessPowerMeterData(powerMeterData);
@@ -98,21 +70,18 @@ public class PowerMeterWatcher extends Observable implements IPowerMeterWatcher 
             setChanged();
             notifyObservers(measure);
             
-
         } catch (UnknownHostException ex) {
             Logger.getLogger(PowerMeterWatcher.class.getName()).log(Level.SEVERE, null, ex);
+            NotifyException(ex);
         } catch (IOException ex) {
             Logger.getLogger(PowerMeterWatcher.class.getName()).log(Level.SEVERE, null, ex);
+            NotifyException(ex);
         } catch (InvalidModbusResponseException ex) {
             Logger.getLogger(PowerMeterWatcher.class.getName()).log(Level.SEVERE, null, ex);
+            NotifyException(ex);
         } catch (Exception ex) {
             Logger.getLogger(PowerMeterWatcher.class.getName()).log(Level.SEVERE, null, ex);
-
-
-
-        /*int lectura = new Random().nextInt(10000);
-        setChanged();
-        notifyObservers(lectura);*/
+            NotifyException(ex);
         }
     }
 
@@ -123,5 +92,13 @@ public class PowerMeterWatcher extends Observable implements IPowerMeterWatcher 
     public void AddObserver(Observer observer) {
         addObserver(observer);
     }
- 
+
+    /**
+     * 
+     * @param ex
+     */
+    private void NotifyException(Exception ex) {
+        setChanged();
+        notifyObservers(ex);
+    }
 }
