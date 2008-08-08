681f03675059fa5d2648a4d058801438bc6f9203
gishac <gishac@c6e2a2cf-2f43-0410-996f-8b6ab9ff9bea>
2008-08-08 05:36:02 +0000

    Cambios en estructura para soportar asignacion de multiples medidores a los usuarios para razones administrativas

--- sicce-api-dataaccess/src/sicce/api/dataaccess/Main.java
+++ sicce-api-dataaccess/src/sicce/api/dataaccess/Main.java
@@ -5,7 +5,11 @@
 
 package sicce.api.dataaccess;
 
-import java.util.Calendar;
+import sicce.api.info.UserPowerMeter;
+import sicce.api.info.interfaces.IPowerMeter;
+import sicce.api.info.interfaces.IUserPowerMeter;
+import sicce.api.info.interfaces.IUserSicce;
+import sicce.api.util.EncryptionProvider;
 
 /**
  *
@@ -14,7 +18,18 @@ import java.util.Calendar;
 public class Main {
  public static void main(String[] args) {
     
+     EncryptionProvider.RegisterHibernateEncryptor();
      
+     IUserSicce user = DataAccessManager.getInstance().getUserDB().findById(1);
+     IPowerMeter powerMeter = DataAccessManager.getInstance().getPowerMeterDB().findById(1);
+     
+     IUserPowerMeter upm = new UserPowerMeter();
+     upm.setUserSicce(user);
+     upm.setPowerMeter(powerMeter);
+     upm.setMonitor(Byte.valueOf("1"));
+     upm.setAssigned(Byte.valueOf("0"));
+     
+     DataAccessManager.getInstance().getUserPowerMeterDB().save(upm);
      
     }
 }
