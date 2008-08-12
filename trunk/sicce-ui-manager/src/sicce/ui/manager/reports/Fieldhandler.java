4220bf17330c51a533dc0273451daf514566c2cd
gishac <gishac@c6e2a2cf-2f43-0410-996f-8b6ab9ff9bea>
2008-08-12 06:12:10 +0000

    Cambios en reportes

---
+++ trunk/sicce-ui-manager/src/sicce/ui/manager/reports/Fieldhandler.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sicce.ui.manager.reports;
+
+/**
+ *
+ * @author gish@c
+ */
+class Fieldhandler {
+
+}
