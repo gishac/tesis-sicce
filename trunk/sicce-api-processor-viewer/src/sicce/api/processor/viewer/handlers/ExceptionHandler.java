b475690e001c2413bf35898d4476e2a3495b6f0b
gishac <gishac@c6e2a2cf-2f43-0410-996f-8b6ab9ff9bea>
2008-08-12 04:52:57 +0000

    Cambios en estructura para soportar asignacion de multiples medidores a los usuarios para razones administrativas

--- trunk/sicce-api-processor-viewer/src/sicce/api/processor/viewer/handlers/ExceptionHandler.java
+++ trunk/sicce-api-processor-viewer/src/sicce/api/processor/viewer/handlers/ExceptionHandler.java
@@ -2,14 +2,25 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package sicce.api.processor.viewer.handlers;
 
 import java.awt.TrayIcon;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
 import javax.swing.DefaultListModel;
+import sicce.api.businesslogic.ParameterBizObject;
+import sicce.api.info.ConstantsProvider;
+import sicce.api.info.interfaces.ILocation;
+import sicce.api.info.interfaces.IParameter;
 import sicce.api.info.interfaces.IPowerMeter;
+import sicce.api.info.interfaces.IUserPowerMeter;
+import sicce.api.info.interfaces.IUserSicce;
 import sicce.api.processor.viewer.observers.ExceptionObserver;
+import sicce.api.util.MailUtil;
 
 /**
  *
@@ -19,16 +30,31 @@ public class ExceptionHandler {
 
     private TrayIcon trayIcon;
     private DefaultListModel exceptionList;
-    
+    private HashMap<String, IParameter> parametersForMail;
+
+    private HashMap<String, IParameter> getparametersForMail() {
+        if (parametersForMail == null) {
+            parametersForMail = new HashMap<String, IParameter>();
+            ParameterBizObject parameterBizObject = new ParameterBizObject();
+            List<IParameter> parameters = parameterBizObject.GetAllParameters();
+            for (IParameter parameter : parameters) {
+                if (parameterBizObject.getParameterCodesForMail().contains(parameter.getParameterKey())) {
+                    parametersForMail.put(parameter.getParameterKey(), parameter);
+                }
+            }
+        }
+        return parametersForMail;
+    }
+
     /**
      * 
      * @param trayIcon
      */
-    public ExceptionHandler(TrayIcon trayIcon, DefaultListModel exceptionList){
+    public ExceptionHandler(TrayIcon trayIcon, DefaultListModel exceptionList) {
         this.trayIcon = trayIcon;
         this.exceptionList = exceptionList;
     }
-    
+
     /**
      * 
      * @param trayIcon
@@ -36,22 +62,45 @@ public class ExceptionHandler {
      */
     public ExceptionObserver getExceptionObserver() {
         return new ExceptionObserver(this);
-        
+
     }
-    
+
     /**
      * 
      * @param ex
      */
-    public void HandleException(Exception ex, IPowerMeter powerMeter){
-         String error = Calendar.getInstance().getTime() + " - Ocurrio un error en el medidor: " + powerMeter.getDescription() + ". ERROR => " +
+    public void HandleException(Exception ex, IPowerMeter powerMeter) {
+        String errorOrigin = Calendar.getInstance().getTime() + " - Ocurrio un error en el medidor: " + powerMeter.getDescription() + ". ERROR => " +
                 ex.getMessage();
-        if(this.trayIcon != null){
-            trayIcon.displayMessage("Error procesando lectura", 
-           "Ocurrio un error en el medidor: " + powerMeter.getDescription() + 
-                    "\n" + "Comuníquese con el Administrador del sistema." ,
-           TrayIcon.MessageType.ERROR);
+        String errorMessage = "Ha ocurrido un error de procesamiento de datos en el medidor : " +
+                powerMeter.getDescription() + " asignado a la dependencia: " + ((ILocation) powerMeter.getLocations().toArray()[0]).getDescription() + ".<br /><br /> <strong>SICCE Monitor <br /> UCSG</strong> ";
+        if (this.trayIcon != null) {
+            trayIcon.displayMessage("Error procesando lectura",
+                    "Ocurrio un error en el medidor: " + powerMeter.getDescription() +
+                    "\n" + "Comuníquese con el Administrador del sistema.",
+                    TrayIcon.MessageType.ERROR);
         }
-        this.exceptionList.addElement(error);
+        SendMail(powerMeter, errorMessage);
+        this.exceptionList.addElement(errorOrigin);
+    }
+
+    /**
+     * 
+     * @param powerMeter
+     */
+    private void SendMail(IPowerMeter powerMeter, String errorMessage) {
+        Properties props = new Properties();
+        props.put("mail.smtps.auth", getparametersForMail().get(ConstantsProvider.MAIL_USE_SSL).getValue());
+        props.put("mail.smtp.host", getparametersForMail().get(ConstantsProvider.SMTP_SERVER).getValue());
+        props.put("mail.smtp.port", getparametersForMail().get(ConstantsProvider.SMTP_PORT).getValue());
+
+        Set<IUserSicce> to = new HashSet<IUserSicce>();
+        for (IUserPowerMeter userPowerMeter : powerMeter.getUserPowerMeters()) {
+            if(userPowerMeter.getUserSicce().getEmail() != null && userPowerMeter.getUserSicce().getEmail().length() > 0)
+                to.add(userPowerMeter.getUserSicce());
+        }
+        MailUtil.SendMail(props, getparametersForMail().get(ConstantsProvider.SMTP_SERVER).getValue(),
+                getparametersForMail().get(ConstantsProvider.MAIL_SENDER).getValue(), "SICCE Error " + Calendar.getInstance().getTime().toString(),
+                to, errorMessage, getparametersForMail().get(ConstantsProvider.MAIL_SENDER_PASSWORD).getValue());
     }
 }
