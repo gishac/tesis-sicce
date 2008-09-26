e517fa99666bd4f87857281a228c3b79dfe3b133
gishac <gishac@c6e2a2cf-2f43-0410-996f-8b6ab9ff9bea>
2008-09-26 04:09:26 +0000

    Correccion de errores

--- sicce-api-dataaccess/src/sicce/api/dataaccess/Main.java
+++ sicce-api-dataaccess/src/sicce/api/dataaccess/Main.java
@@ -4,19 +4,6 @@
  */
 package sicce.api.dataaccess;
 
-import java.sql.CallableStatement;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import oracle.jdbc.OracleTypes;
-import sicce.api.info.UserPowerMeter;
-import sicce.api.info.interfaces.IPowerMeter;
-import sicce.api.info.interfaces.IUserPowerMeter;
-import sicce.api.info.interfaces.IUserSicce;
-import sicce.api.util.EncryptionProvider;
-
 /**
  *
  * @author gish@c
@@ -24,25 +11,6 @@ import sicce.api.util.EncryptionProvider;
 public class Main {
 
     public static void main(String[] args) {
-        try {
-
-
-
-
-            //DataAccessManager.getInstance().getUserPowerMeterDB().save(upm);
-            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
-
-            Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:Database", "gisbert", "gisbert");
-            
-            String query = "begin ?:=demo.sendsms(); end;";
-            CallableStatement stmt = conn.prepareCall(query);
-
-            //stmt.setInt(1,1);
-            stmt.execute();
-
-        } catch (SQLException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
+       
     }
 }
