b12044f23e56a3f58804eecc19123c7eebe8e422
gishac <gishac@c6e2a2cf-2f43-0410-996f-8b6ab9ff9bea>
2008-10-01 07:55:47 +0000

    Soporte para rubros

---
+++ sicce-api-util/src/sicce/api/util/JTextFieldNumber.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sicce.api.util;
+
+import java.awt.Toolkit;
+import java.util.Locale;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.PlainDocument;
+
+/**
+ *
+ * @author gish@c
+ */
+public class JTextFieldNumber extends PlainDocument {
+private int maxLength = 9999;
+
+    public JTextFieldNumber() {
+
+    }
+
+    public JTextFieldNumber(int maxLength) {
+        this.maxLength = maxLength;
+    }
+
+    @Override
+    public void insertString(int offset, String str, javax.swing.text.AttributeSet a) throws BadLocationException {
+        try {
+            Double.parseDouble(str);
+            if (getLength() + str.length() > maxLength) {
+                Toolkit.getDefaultToolkit().beep();
+                return;
+            } else {
+                try {
+                    super.insertString(offset, str, a);
+                } catch (NumberFormatException exp) {
+                    Toolkit.getDefaultToolkit().beep();
+                    return;
+                }
+            }
+        } catch (Exception ex) {
+            if (str.length() > 0) {
+                Toolkit.getDefaultToolkit().beep();
+                str = str.substring(0, str.length() - 1);
+                super.insertString(offset, str, a);
+            } else {
+                return;
+            }
+        }
+    }
+}
